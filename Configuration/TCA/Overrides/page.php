e64507b83303db1343af679a96749a50ffa69719
Christian Hackl <hackl.chris@googlemail.com>
2023-07-12 13:28:21 +0200

    [TASK] code format

--- Configuration/TCA/Overrides/page.php
+++ Configuration/TCA/Overrides/page.php
@@ -1,11 +1,13 @@
 <?php
 defined('TYPO3') || die();
 
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 call_user_func(function() {
     $extensionKey = 'hh_simple_job_posts';
 
     // make PageTsConfig selectable
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    ExtensionManagementUtility::registerPageTSConfigFile(
         $extensionKey,
         'Configuration/TsConfig/AllPage.typoscript',
         'Simple Job Posts Page TS'
