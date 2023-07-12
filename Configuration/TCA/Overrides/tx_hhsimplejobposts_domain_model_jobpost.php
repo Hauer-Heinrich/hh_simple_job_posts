ec7e65ed6a9119103451fc62540790aa2b8a4c8e
Christian Hackl <hackl.chris@googlemail.com>
2023-07-12 13:27:33 +0200

    [TASK] delete overrides domain model jobpost - depricated makeCategorizable

--- Configuration/TCA/Overrides/tx_hhsimplejobposts_domain_model_jobpost.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
-    'hh_simple_job_posts',
-    'tx_hhsimplejobposts_domain_model_jobpost'
-);
